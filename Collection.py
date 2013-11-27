635e620f84e376cc6098cb01e196a35e04cc6869
RyanChan0220 <chenrong0220@gmail.com>
2013-11-27 23:20:45 +0800

    test python-excel

--- Collection.py
+++ Collection.py
@@ -1,4 +1,28 @@
 __author__ = 'Ryan'
 
-fNum = open('3DLottery.txt', 'r')
-line = fNum.readline()
+from mmap import mmap, ACCESS_READ
+from xlrd import open_workbook
+
+book = open_workbook('20020101-20130501.xls')
+
+print book.nsheets
+
+for sheet_index in range(book.nsheets):
+    print book.sheet_by_index(sheet_index)
+
+print book.sheet_names()
+
+for sheet_name in book.sheet_names():
+    print book.sheet_by_name(sheet_name)
+
+for sheet in book.sheets():
+    print sheet
+
+
+#print open_workbook('20020101-20130501.xls')
+#
+#with open('20020101-20130501.xls', 'rb') as f:
+#    print open_workbook(file_contents=mmap(f.fileno(), 0, access=ACCESS_READ))
+#
+#aString = open('20020101-20130501.xls', 'rb').read()
+#print open_workbook(file_contents=aString)
